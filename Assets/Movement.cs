dff7f8e57c9df7740b3d4af12754e79aa65607e6
Jona <jona.lam@yahoo.com>
2022-09-22 12:09:56 +0200

    Update Movement.cs

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    
+    private Rigidbody2D _rb;
+    [SerializeField] private float speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        _rb.velocity = Vector2.right * Input.GetAxisRaw("Horizontal") * speed;
     }
 }
